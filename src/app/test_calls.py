dc399a960694b65c401e5c1c7eaf6c5ecdd2d60b
day007V <dayventrice@hotmail.com>
2025-06-22 11:38:38 +0200

    Update test_calls.py

--- src/app/test_calls.py
+++ src/app/test_calls.py
@@ -2,8 +2,8 @@ import requests
 
 URL = "http://127.0.0.1:5000/predict/{}"
 
-# Exemple de features d'una senyal valors del CSV
-examplen = [140.5625,55.68378214,-0.234571412,-0.699648398,3.199832776,19.11042633,7.975531794,74.24222492,0]  # primers 
+# Exemple de features d'una senyal valors del CSV 
+examplen = [140.5625,55.68378214,-0.234571412,-0.699648398,3.199832776,19.11042633,7.975531794,74.24222492]  # 8 primers valors del dataset
 
 payload = {"features": examplen}
 
